c1e50b7481ef4e4e98491ff914833dbc362d35fc
HWIJakob <134200282+HWIJakob@users.noreply.github.com>
2023-05-21 21:02:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
 
-require 'PHPMailer/PHPMailer.php';
-require 'PHPMailer/Exception.php';
-require 'PHPMailer/SMTP.php';
+require 'smile/PHPMailer.php';
+require 'smile/Exception.php';
+require 'smile/SMTP.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
